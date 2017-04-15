b4e3026aa5693625e047649940d8fe97d0f821f4
mName <mr.mytab@gmail.com>
2017-04-15 11:36:12 +0300

    was added some comments

--- MultyThreading/ThreadingTest/src/com/mname/thread/testing/ThreadDemo.java
+++ MultyThreading/ThreadingTest/src/com/mname/thread/testing/ThreadDemo.java
@@ -3,6 +3,7 @@ package com.mname.thread.testing;
 public class ThreadDemo {
 
 	public static void main(String[] args) {
+		// creating some threads to test
 		for (int i = 1; i < 4; i++) {
 			String nameSrc = "Поток " + i;
 			new NewTypicalThread(nameSrc);
